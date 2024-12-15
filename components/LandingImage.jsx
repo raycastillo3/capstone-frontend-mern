df7bb1a584a448449a7534d9ad7185e55c63bd88
raycastillo3 <60750068+raycastillo3@users.noreply.github.com>
2024-12-15 17:48:59 -0500

    frontend

---
+++ components/LandingImage.jsx
@@ -0,0 +1,8 @@
+export function LandingImage () {
+    return (
+        <div className="landingPageContainer">
+            <img className="landingImage" src="https://shorturl.at/aZNRG"/>
+            <div className="landingPageTitle"><h1>Snack Overflow</h1></div>
+        </div>
+    )
+}
---
+++ components/Navbar.jsx
@@ -0,0 +1,27 @@
+import { useState, useEffect } from "react";
+
+export function Navbar() {
+  const [showNavbar, setShowNavbar] = useState(false);
+  const [prevScrollPos, setPrevScrollPos] = useState(0);
+
+  const handleScroll = () => {
+    const currentScrollPos = window.scrollY;
+    if (currentScrollPos === 0) {
+      setShowNavbar(false);
+    } else if (currentScrollPos > prevScrollPos) {
+      setShowNavbar(true);
+    }
+    setPrevScrollPos(currentScrollPos);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [prevScrollPos]);
+
+  return (
+    <div className={`navbar ${showNavbar ? "show" : "hide"}`}>
+      <img src="../public/snackoverflow.png" alt="logo" />
+    </div>
+  );
+}
