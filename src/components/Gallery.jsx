ea5a13941c902dc0a0dd03e071bb2947f1dcfef8
raycastillo3 <60750068+raycastillo3@users.noreply.github.com>
2024-12-19 13:08:03 -0500

    added knew images

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -18,28 +18,28 @@ export function Gallery() {
     name: "Wolfgang's Steakhouse",
     rating: 4,
     price: 4,
-    imageSrc: "https://d1ralsognjng37.cloudfront.net/028932d2-8d93-41a5-8796-2b74a7ea1822.jpeg",
+    imageSrc: "https://d1ralsognjng37.cloudfront.net/7896b961-52d2-4fab-a37e-6cb8526f6736.jpeg",
 },
     {
     id: 1,
     name: "Wolfgang's Steakhouse",
     rating: 4,
     price: 4,
-    imageSrc: "https://d1ralsognjng37.cloudfront.net/028932d2-8d93-41a5-8796-2b74a7ea1822.jpeg",
+    imageSrc: "https://d1ralsognjng37.cloudfront.net/d29a9000-fcbe-49ea-acd0-c039bf85d4f2.webp",
 },
     {
     id: 1,
     name: "Wolfgang's Steakhouse",
     rating: 4,
     price: 4,
-    imageSrc: "https://d1ralsognjng37.cloudfront.net/028932d2-8d93-41a5-8796-2b74a7ea1822.jpeg",
+    imageSrc: "https://duyt4h9nfnj50.cloudfront.net/resized/1528911206513-w240-f9.jpg",
 },
     {
     id: 1,
     name: "Wolfgang's Steakhouse",
     rating: 4,
     price: 4,
-    imageSrc: "https://d1ralsognjng37.cloudfront.net/028932d2-8d93-41a5-8796-2b74a7ea1822.jpeg",
+    imageSrc: "https://d1ralsognjng37.cloudfront.net/e08598ef-9897-44bc-b439-a10d20b7f1e9.jpeg",
 },
 
 ]
